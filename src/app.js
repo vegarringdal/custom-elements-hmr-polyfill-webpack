e358c4dc16a379171a4ed76319aed3bcc594eaf9
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-07-06 18:03:35 +0200

    chore: simple lit-html

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-export function run() {
+import "@vaadin/vaadin-date-picker";
 
+export function run() {
   console.log("<---------Starting app----------->");
 
   class SimpleGreeting extends HTMLElement {
     connectedCallback() {
-      this.innerHTML = "innerhtml";
+      this.innerHTML = `<!-- template -->
+        <vaadin-date-picker label="Labels"></vaadin-date-picker>
+        <vaadin-date-picker placeholder="Placeholder"></vaadin-date-picker>
+        <vaadin-date-picker value="1990-01-30"></vaadin-date-picker>`;
     }
   }
   customElements.define("app-root", SimpleGreeting);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-/* import "../node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js";
-import "../node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js"; */
-
-
-import "document-register-element";
-
-if (!window.globalThis) {
-  window.globalThis = window;
-}
 import { applyPolyfill, ReflowStrategy } from "custom-elements-hmr-polyfill";
 applyPolyfill(ReflowStrategy.NONE);
 import { run } from "./app";
